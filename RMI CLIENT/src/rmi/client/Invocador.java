12f139ef37d228b62e7eaae634f504b2328ae69b
kevinmarincardenas <kevinmarin1012@gmail.com>
2025-05-22 09:03:29 -0500

    ola

--- RMI CLIENT/src/rmi/client/Invocador.java
+++ RMI CLIENT/src/rmi/client/Invocador.java
@@ -15,9 +15,16 @@ public class Invocador {
 
     private final IServer server;
     private final String name;
-   
+    private static Invocador instancia;
 
-    public Invocador(String name) throws Exception {
+    public static synchronized Invocador getInstance(String name) throws Exception {
+        if (instancia == null) {
+            instancia = new Invocador(name); // Crea la instancia solo si no existe
+        }
+        return instancia;
+    }
+
+    private Invocador(String name) throws Exception {
         this.name = name;
         Registry reg = LocateRegistry.getRegistry("LocalHost", 3232);
         this.server = (IServer) reg.lookup("rmiserver");
@@ -49,15 +56,13 @@ public class Invocador {
 //            }
 //        }
     }
-    
-   public List<String> getConnectedUsers() throws RemoteException {
-       
-            // Llama al método remoto del servidor para obtener la lista
-            return server.getConnectedUsers();
-      
-   }
-           
-    
+
+    public List<String> getConnectedUsers() throws RemoteException {
+
+        // Llama al método remoto del servidor para obtener la lista
+        return server.getConnectedUsers();
+
+    }
 
     private void pollMessages() {
         try {
