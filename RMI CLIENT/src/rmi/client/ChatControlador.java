1da728b2637b5f35fff768425e7843ae4fc20fbe
Gian <gkabril19@gmail.com>
2025-06-11 15:12:48 -0500

    Implementacion de metodo cerrarRecursos

--- RMI CLIENT/src/rmi/client/ChatControlador.java
+++ RMI CLIENT/src/rmi/client/ChatControlador.java
@@ -16,19 +16,20 @@ import java.util.logging.Logger;
  * @author Usuario
  */
 public class ChatControlador {
-
+    
     private ChatGUI vistaChat;
     private Cliente fachada = Cliente.getInstance();
     private ThreadLatidosCliente hiloPing;
     private ExecutorService executorService = Executors.newCachedThreadPool();
-
+    
     public ChatControlador(ChatGUI vistaChat) throws Exception {
         this.vistaChat = vistaChat;
-
+        
         actualizarChats();
         actualizarListadoUsuarios();
         iniciarHiloPing();
-
+        Runtime.getRuntime().addShutdownHook(new Thread(this::cerrarRecursosPing));
+        
         vistaChat.getBtbSalir().addActionListener(e -> {
             try {
                 desconectarUsuario();
@@ -37,40 +38,40 @@ public class ChatControlador {
             }
         });
     }
-
+    
     private void actualizarChats() throws RemoteException {
         ThreadChatActualizar actualizar = new ThreadChatActualizar(vistaChat);
         actualizar.start();
     }
-
+    
     private void actualizarListadoUsuarios() throws RemoteException {
         ThreadChatListUsuarios actualizar = new ThreadChatListUsuarios(vistaChat);
         actualizar.start();
     }
-
+    
     private void enviarMensajeThread(String destino, String texto) throws RemoteException {
         ThreadEnviarMensaje enviar = new ThreadEnviarMensaje(vistaChat, destino, texto);
         enviar.start();
     }
-
+    
     public void desconectarUsuario() throws RemoteException {
         fachada.desconectarUsuario();
         vistaChat.setVisible(false);
     }
-
+    
     void enviarMensajeDirecto(String destino, String texto) throws RemoteException {
         enviarMensajeThread(destino, texto);
     }
-
+    
     public void enviarMensajeGlobal(String texto) throws RemoteException {
         ThreadEnviarMensajeGlobal envio = new ThreadEnviarMensajeGlobal(fachada.getName(), texto);
         envio.start();
     }
-
+    
     public void iniciarHiloPing() {
         hiloPing = new ThreadLatidosCliente(fachada.getName());
         executorService.submit(hiloPing);
-
+        
     }
 
     // Este metodo es para gestionar el hilo de latidos, será llamado por <iniciarHiloPing()>
@@ -80,7 +81,7 @@ public class ChatControlador {
             /* Método para parar el hilo de forma segura, 
                                    es llamado desde la clase ThreadLatidosCliente*/
         }
-
+        
         if (executorService != null && !executorService.isShutdown()) {
             executorService.shutdown();
             try {
@@ -92,5 +93,5 @@ public class ChatControlador {
             }
         }
     }
-
+    
 }
